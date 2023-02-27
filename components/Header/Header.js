f57327f097b97fb755069bb49bd8b82e451fe15c
tarekaya <118102792+tarekaya@users.noreply.github.com>
2023-02-27 10:21:14 +0100

    1 file modified

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Header</div>;
+  return;
+  <div className="header">
+    <h1>Ayad Academy</h1>
+    <p>salem alikoum</p>
+  </div>;
 }
