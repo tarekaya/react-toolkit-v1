3f1cfec660feb218e769185512b7e2cd51c3b0e7
tarekaya <118102792+tarekaya@users.noreply.github.com>
2023-03-03 10:20:11 +0100

    2 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import './index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
