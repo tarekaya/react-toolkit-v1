ba6eff97904c9105077297584cc2cdda1ae2b440
tarekaya <118102792+tarekaya@users.noreply.github.com>
2023-03-03 12:02:16 +0100

    2 files modified, 1 added, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './style.css';
+import './App.css';
 import Header from './components/Header/Header.js';
 import Form from './components/Form/Form';
 import Sidebar from './components/Sidebar/Sidebar';
--- components/Form/Form.js
+++ components/Form/Form.js
@@ -5,13 +5,19 @@ export default function Form() {
   return (
     <form>
       <div className="form-group">
-        <input type="text" placeholder="enter name" />
+        <input type="text" placeholder="Enter Name" />
       </div>
       <div className="form-group">
-        <input type="text" placeholder="enter lastName" />
+        <input type="text" placeholder="Enter LastName" />
       </div>
       <div className="form-group">
-        <input type="submit" placeholder="enter city" />
+        <input type="text" placeholder="City" />
+      </div>
+      <div className="form-group">
+        <input type="email" placeholder="Email" />
+      </div>
+      <div className="form-group">
+        <input type="submit" placeholder="Add User" />
       </div>
     </form>
   );
