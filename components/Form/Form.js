d1ea8027d8fa64d5a6bb96bf11c4c5440b59b24a
tarekaya <118102792+tarekaya@users.noreply.github.com>
2023-03-03 14:22:05 +0100

    2 files modified

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -2,19 +2,42 @@ import React from 'react';
 import './Form.css';
 
 export default function Form() {
+  const [name, setName] = useState('');
+  const [firstName, setfirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [city, setCity] = useState('');
+  handleSubmit = (e) => {
+    e.pre
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(e)}>
       <div className="form-group">
-        <input type="text" placeholder="Enter Name" />
+        <input
+          type="text"
+          placeholder="Enter Name"
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <div className="form-group">
-        <input type="text" placeholder="Enter LastName" />
+        <input
+          type="text"
+          placeholder="Enter LastName"
+          onChange={(e) => setfirstName(e.target.value)}
+        />
       </div>
       <div className="form-group">
-        <input type="text" placeholder="City" />
+        <input
+          type="text"
+          placeholder="City"
+          onChange={(e) => setCity(e.target.value)}
+        />
       </div>
       <div className="form-group">
-        <input type="email" placeholder="Email" />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-group">
         <input type="submit" placeholder="Add User" />
--- index.tsx
+++ index.tsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { store } from './redux/store';
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
