6898fa55b3cea654ee90cda9625036845d06df1f
tarekaya <118102792+tarekaya@users.noreply.github.com>
2023-03-03 10:42:05 +0100

    3 files modified

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import './Form.css';
 
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <form>
+      <div className="form-group">
+        <input type="text" placeholder="enter name" />
+      </div>
+      <div className="form-group">
+        <input type="text" placeholder="enter lastName" />
+      </div>
+      <div className="form-group">
+        <input type="submit" placeholder="enter city" />
+      </div>
+    </form>
+  );
 }
